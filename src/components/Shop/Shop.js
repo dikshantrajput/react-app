8c1da55f3bafb405f04e5ba6a2444924788e54cc
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-08-24 15:34:47 +0600

    updates

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Shop.css'
+import fakeData from '../../fakeData';
+import Product from '../Product/Product';
 
 const Shop = () => {
+    const shop1 = fakeData.slice(0,10);
+
+    const [products, setProducts] = useState(shop1);
+    
+
     return (
-        <div>
-            <h1>This Is Shop</h1>
+        <div className="product-container">
+            <div className="shop-container">
+               {
+                    products.map(pd => <Product product={pd}></Product>)
+               }
+            </div>
+            <div className="cart-container">
+                <p>This is shop area</p>
+            </div>
         </div>
     );
 };
