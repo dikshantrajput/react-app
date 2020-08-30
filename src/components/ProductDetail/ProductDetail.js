b87675b58661c7a8a0318f511579f8529d9f915d
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-08-30 13:49:45 +0600

    updates

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './ProductDetail.css'
+import { useParams } from 'react-router-dom';
+import fakeData from '../../fakeData';
+import Product from '../Product/Product';
 
 const ProductDetail = () => {
+    const {productKey} = useParams();
+    const product = fakeData.find(pd => pd.key === productKey);
+
+    console.log(product);
+    
+    
     return (
         <div>
             <h1>Product Detail coming Soon </h1>
+            <Product product={product}></Product>
         </div>
     );
 };
