152cda52307cd2e5836e57bcbe786565491e168f
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-08-30 12:50:25 +0600

    updates

--- src/App.js
+++ src/App.js
@@ -8,12 +8,29 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import Order from './components/Order/Order';
+import Inventory from './components/Inventory/Inventory';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/order">
+            <Order></Order>
+          </Route>
+          <Route path="/manage">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="/">
+            <Shop></Shop>
+          </Route>          
+        </Switch>
+      </Router>
     </div>
   );
 }
