8b2c5d0b30d4e7c122c20e3dc67e483de2c74a1e
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-09-02 00:23:15 +0600

    update

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -13,10 +13,19 @@ const Shop = () => {
     const [cart , setCart] = useState([])
 
     const handleProduct = (product) => {
-        const newCart = [...cart, product]
+        const sameProduct = cart.find(pd => pd.key === product.key);
+        let count = 1;
+        let newCart;
+        if(sameProduct){
+            count = sameProduct.quantity + 1;
+            sameProduct.quantity = count;
+            const others = cart.filter(pd => pd.key !== product.key) 
+            newCart = [...others , sameProduct]
+        } else{
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
         setCart(newCart);
-        const sameProduct = newCart.filter(pd => pd.key === product.key)
-        const count = sameProduct.length;
         addToDatabaseCart(product.key, count)
     }
     
