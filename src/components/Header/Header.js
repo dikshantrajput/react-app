0d5975bf4cb7af13d55de6a978fbca168828ca2c
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-09-01 20:21:35 +0600

    updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,14 +4,7 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div className="header">
-            <img src={logo} alt=""/>
-            <nav>
-                <a href="/shop">Shop</a>
-                <a href="/order">Order Review</a>
-                <a href="/manage">Manage Inventory</a>
-            </nav>
-        </div>
+
     );
 };
 
