225ef8ba99a2fc93bc43e6cf09a2bd0c67f4c58e
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-09-13 17:16:13 +0600

    updates

--- src/components/LogIn/firebase.config.js
+++ src/components/LogIn/firebase.config.js
@@ -1,3 +1,10 @@
 const firebaseConfig = {
+    apiKey: "AIzaSyBBilDhVhJ4sU4a18mkxTiEOyNi9BIxBJ0",
+    authDomain: "ema-jhon-simple-4fadd.firebaseapp.com",
+    databaseURL: "https://ema-jhon-simple-4fadd.firebaseio.com",
+    projectId: "ema-jhon-simple-4fadd",
+    storageBucket: "ema-jhon-simple-4fadd.appspot.com",
+    messagingSenderId: "287715917867",
+    appId: "1:287715917867:web:c88ff4b41aea42ff3cea6e"
 };
 export default firebaseConfig;  
